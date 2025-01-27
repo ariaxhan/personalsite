f6f8a0c769c4b1878375f2c7e5d9a0437605e1bd
ariahan <ariaxhan@gmail.com>
2025-01-27 08:05:03 -0800

    cleaning up

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 // app/page.tsx
-import dynamic from 'next/dynamic';
-import Navbar from './components/Navbar';
+import dynamic from "next/dynamic";
+import Navbar from "./components/Navbar";
 
 // Dynamically import components
-const Hero = dynamic(() => import('./components/Hero'), {
-  loading: () => <div className="h-screen" /> // Placeholder while loading
+const Hero = dynamic(() => import("./components/Hero"), {
+  loading: () => <div className="h-screen" />, // Placeholder while loading
 });
 
-const About = dynamic(() => import('./components/About'), {
-  loading: () => <div className="min-h-screen" />
+const About = dynamic(() => import("./components/About"), {
+  loading: () => <div className="min-h-screen" />,
 });
 
-const Contact = dynamic(() => import('./components/Contact'), {
-  loading: () => <div className="min-h-screen" />
+const Contact = dynamic(() => import("./components/Contact"), {
+  loading: () => <div className="min-h-screen" />,
 });
 
 export default function Home() {
@@ -20,7 +20,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      <About />
+      {/* <About /> */}
       <Contact />
     </>
   );
