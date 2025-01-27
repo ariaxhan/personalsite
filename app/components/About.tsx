ddc4c5bfec2be691e0c410efc47a26899a5486c0
ariahan <ariaxhan@gmail.com>
2025-01-26 22:45:10 -0800

    updating some words

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,15 +3,17 @@
 
 export default function About() {
   return (
-    <section id="about" className="min-h-screen flex flex-col justify-center items-center px-8 py-32 relative overflow-hidden">
+    <section
+      id="about"
+      className="min-h-screen flex flex-col justify-center items-center px-8 py-32 relative overflow-hidden"
+    >
       <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-pink-200 via-white to-transparent"></div>
       <div className="max-w-5xl mx-auto text-center animate-fade-in">
         <h2 className="text-6xl lg:text-7xl font-bold mb-10">about me</h2>
         <p className="mt-6 text-lg lg:text-xl text-gray-600 max-w-3xl animate-slide-in">
-          i'm the ceo and co-founder of brink labs, where we build privacy-first mental health software that leverages ai and biometrics to deliver personalized insights.
-          
-          <br />     <br />   
-          with a passion for creating intuitive, impactful technology, i've led multiple award-winning projects and thrive at the intersection of innovation and user experience.
+          i'm the ceo and co-founder of brink labs.
+          <br /> <br />
+          i'm passionate about creating intuitive, impactful technology.
         </p>
       </div>
     </section>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/no-unescaped-entities */
 
 "use client";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Hero() {
   return (
     <section className="h-screen flex flex-col justify-center items-center px-8 text-center relative overflow-hidden">
       <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-pink-200 via-white to-transparent"></div>
-     <h1 className="text-6xl lg:text-7xl font-extrabold text-gray-800 leading-tight animate-fade-in">
+      <h1 className="text-6xl lg:text-7xl font-extrabold text-gray-800 leading-tight animate-fade-in">
         aria han
       </h1>
       <p className="mt-6 text-lg lg:text-xl text-gray-600 max-w-3xl animate-slide-in">
-        startup founder and software engineer building AI-powered solutions for mental health.
+        startup founder and software engineer.
       </p>
       <div className="mt-8 flex flex-col sm:flex-row gap-4 animate-fade-in">
         <Link href="#contact">
